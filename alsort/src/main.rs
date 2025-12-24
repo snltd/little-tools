b30abfa53e4b886b8e7fe703c4f645b082b3313f
Rob Fisher <services@id264.net>
2025-12-24 00:10:45 +0000

    fix tests on macos

--- alsort/src/main.rs
+++ alsort/src/main.rs
@@ -137,7 +137,7 @@ mod test {
         let temp = Utf8TempDir::new().unwrap();
         temp.child("a_file.txt").touch().unwrap();
         temp.child("abc").create_dir_all().unwrap();
-        let file_under_test = temp.path().join("abc");
+        let file_under_test = temp.path().canonicalize_utf8().unwrap().join("abc");
 
         assert!(file_under_test.exists());
 
@@ -145,7 +145,7 @@ mod test {
             !process(
                 &file_under_test,
                 &Cli {
-                    root: temp.path().to_path_buf(),
+                    root: temp.path().canonicalize_utf8().unwrap().to_path_buf(),
                     verbose: false,
                     group: true,
                     files: vec![],
